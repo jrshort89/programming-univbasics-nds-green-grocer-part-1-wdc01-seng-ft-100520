2394be2f6a33f49097e06618759a6a6e335f3b3f
Jacob Short <jrshort89@gmail.com>
2020-10-01 20:26:48 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,8 +3,8 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   collection.each { |x|
-    
-    
+    if x[:item] == name
+      return x
   }
 end
 
