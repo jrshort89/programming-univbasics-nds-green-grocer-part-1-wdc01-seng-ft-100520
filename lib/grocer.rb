ef4f4eea98dde5947cdf9954ab1fd2717e424e6e
Jacob Short <jrshort89@gmail.com>
2020-10-01 20:19:58 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,11 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-  
+  for elm in collection do
+    if name == elm
+      return elm
+    end
+  end
 end
 
 def consolidate_cart(cart)
